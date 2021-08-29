8cdca7b53c91f173f121108d8353eb5602d90217
phoon884 <67638051+phoon884@users.noreply.github.com>
2021-08-30 00:09:04 +0700

    change

--- src/User.py
+++ src/User.py
@@ -9,6 +9,7 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import create_refresh_token
 from flask_jwt_extended import set_access_cookies
 from flask_jwt_extended import unset_jwt_cookies
+from flask_jwt_extended import get_csrf_token
 
 
 class User:
@@ -54,8 +55,9 @@ class User:
         if user and pbkdf2_sha256.verify(request.json['password'], user['password']):
             del user['name']
             del user['password']
-            response = jsonify("msg", "login successful")
             access_token = create_access_token(identity=user)
+            csrf_token = get_csrf_token(access_token)
+            response = jsonify(msg= "login successful",csrf_token=csrf_token)
             print(access_token)
             set_access_cookies(response, access_token)
             return response
